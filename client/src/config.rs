aeaa4d99e34444974b4d74520ce5dcc6c656e4dc
Sebastian Pütz <me@sebpuetz.eu>
2022-12-18 22:09:28 +0100

    Missing config

---
+++ client/src/config.rs
@@ -0,0 +1,18 @@
+use clap::Parser;
+use uuid::Uuid;
+
+#[derive(Parser)]
+#[clap(name = "voice-client")]
+#[clap(author, version, about, long_about = None)]
+pub struct Config {
+    #[clap(long, default_value = "ws://localhost:33332")]
+    pub ws_endpoint: String,
+    #[clap(long)]
+    pub room_id: Option<Uuid>,
+    #[clap(long, default_value = "Foo")]
+    pub name: String,
+    #[clap(long)]
+    pub deaf: bool,
+    #[clap(long)]
+    pub mute: bool,
+}
